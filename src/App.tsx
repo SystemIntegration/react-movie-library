28e3e8e1bf65337eeac10a46d536d3552660c429
Rutvik Barot <brutvik@systemintegration.in>
2023-04-26 12:22:18 +0530

    Optimize code, prepare one file and add Strings and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { Table, TableCell, TableRow } from '@mui/material';
 import { useEffect, useState } from 'react';
 import "./App.css"
 import image from "./bg.png"
-
+import ErrorImage from "./remove.png"
+import { string, style } from './utils';
 
 function App() {
   const [search, setSearch] = useState("")
 
+  // Method for set search text from input field
   const handleSearch = (value: any) => {
     setSearch(value)
   }
@@ -29,20 +31,21 @@ function App() {
 
   const [data, setData] = useState<MovieData>({
     Title: "",
-    imdbVotes: "",
-    Year: "",
     Poster: "",
     Language: "",
     Runtime: "",
     Director: "",
     Plot: "",
+    Error: "",
     Actors: "",
     Genre: "",
-    Error: "",
+    Year: "",
     Rated: "",
+    imdbVotes: "",
     Ratings: {},
   });
 
+  // Method for get data from searched data
   useEffect(() => {
     async function fetchData() {
       const response = await fetch(`http://www.omdbapi.com/?t=${search}&apikey=da47b56d`);
@@ -53,10 +56,10 @@ function App() {
     fetchData();
   }, [search]);
 
+  // Method for get hours and minutes from total minutes
   function minToHour(num: any) {
     const hours = Math.floor(num / 60);
     const minutes = num % 60;
-
     return (`${hours} h ${minutes} m`)
   }
 
@@ -69,19 +72,19 @@ function App() {
       <br />
       {
         search.length > 0 ? (data.Title ?
-          <div style={{ display: 'flex', justifyContent: 'space-around', margin: '0 3rem', alignItems: 'center' }}>
-            <div style={{ textAlign: 'left' }}>
-              <pre style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '2rem', margin: "0.5rem 0" }} >
+          <div className='mainDiv'>
+            <div className='subDivForDataL'>
+              <pre style={{ fontSize: '2rem' }} >
                 {data.Title}
               </pre>
-              <pre style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.2rem', margin: "0.5rem 0", color: 'gray' }} >
+              <pre style={{ fontSize: '1.2rem', color: 'gray' }} >
                 {data.Year} | {data.Rated} | {data.Runtime == "N/A" ? "N/A" : minToHour(data.Runtime.split(" ")[0])}
               </pre>
-              <img src={data.Poster} alt="" style={{ textAlign: 'center', margin: '0.5rem 0', borderRadius: '12px', height: '25rem', width: '20rem' }} />
+              <img className='img' src={data.Poster} alt="" />
               {data.Ratings.length > 0 && (data.Ratings[0].Value) !== "N/A" &&
-                <pre style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.6rem', margin: "0.5rem 0", fontWeight: 'bold' }} >
+                <pre style={{ fontSize: '1.6rem', fontWeight: 'bold' }} >
                   &#x2B50; {data.Ratings.length > 0 && (data.Ratings[0].Value).split("/")[0]} &nbsp;
-                  <span style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem', margin: "0.5rem 0", fontWeight: '1', color: 'gray' }} >
+                  <span>
                     /{data.Ratings.length > 0 && (data.Ratings[0].Value).split("/")[1]} |&nbsp;
                     {Math.round(parseInt(data.imdbVotes))}K
                   </span>
@@ -90,59 +93,42 @@ function App() {
             <div>
               <Table style={{ width: '80%', fontSize: '5rem', marginLeft: '5rem' }}>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Actors
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
-                    {data.Actors}
-                  </TableCell>
+                  <TableCell style={style.tableHead}>{string.Actors}</TableCell>
+                  <TableCell style={style.tableData}>{data.Actors}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Genre
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
-                    {data.Genre}
-                  </TableCell>
+                  <TableCell style={style.tableHead}>{string.Genre}</TableCell>
+                  <TableCell style={style.tableData}>{data.Genre}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Language
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
-                    {data.Language}
-                  </TableCell>
+                  <TableCell style={style.tableHead}>{string.Language}</TableCell>
+                  <TableCell style={style.tableData}>{data.Language}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Director
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
-                    {data.Director}
-                  </TableCell>
+                  <TableCell style={style.tableHead}>{string.Director}</TableCell>
+                  <TableCell style={style.tableData}>{data.Director}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Description
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
-                    {data.Plot}
-                  </TableCell>
+                  <TableCell style={style.tableHead}>{string.Description}</TableCell>
+                  <TableCell style={style.tableData}>{data.Plot}</TableCell>
                 </TableRow>
                 <TableRow>
-                  <TableCell style={{ fontFamily: 'Times New Roman', fontWeight: 'bold', borderBottom: 0, fontSize: '1.4rem', color: 'gray' }} >
-                    Trailer
-                  </TableCell>
-                  <TableCell style={{ fontFamily: 'Times New Roman', borderBottom: 0, fontSize: '1.4rem' }} >
+                  <TableCell style={style.tableHead}>{string.Trailer}</TableCell>
+                  <TableCell style={style.tableData}>
                     <a href={`https://www.youtube.com/results?search_query=${data.Title}+trailer`} target='_blank' style={{ textDecoration: 'none' }}>{data.Title}</a>
                   </TableCell>
                 </TableRow>
               </Table>
             </div>
-          </div> : <h2 style={{ textAlign: 'center' }}>{data.Error}</h2>) :
-          <div style={{ textAlign: 'center' }}>
-          <img src={image} alt="" style={{ textAlign: 'center', margin: '0.5rem 0', borderRadius: '12px', height: '25rem', width: '20rem' }} />
-          <h2 >To search for a movie, please enter its name in the search field.</h2>
+          </div> :
+          <div className='subDivForError'>
+            <img className='img' src={ErrorImage} alt="Error" />
+            <h2 className='subDivForError'>{data.Error}</h2>
+          </div>
+        ) :
+          <div className='subDivForError'>
+            <img className='img' src={image} alt="" />
+            <h2 >To search for a movie, please enter its name in the search field.</h2>
           </div>
       }
     </div>
---
+++ src/utils.ts
@@ -0,0 +1,23 @@
+// Method for define style in main screen
+export const style: any = {
+    tableHead: {
+        fontFamily: 'Times New Roman', fontWeight: 'bold',
+        borderBottom: 0,
+        fontSize: '1.4rem',
+        color: 'gray',
+    },
+    tableData: {
+        fontFamily: 'Times New Roman', borderBottom: 0,
+        fontSize: '1.4rem',
+    },
+}
+
+// Method for define strings in main screen
+export const string: any = {
+    Actors : 'Actors',
+    Genre : 'Genre',
+    Language : 'Language',
+    Director : 'Director',
+    Description : 'Description',
+    Trailer : 'Trailer',
+}
