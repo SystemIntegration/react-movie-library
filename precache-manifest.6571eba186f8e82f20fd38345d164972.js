955b1b88746173b41a9f8d7416d3a48bd3309552
Hasmukh Barochiya <hasmukhb@systemintegration.in>
2023-05-29 18:39:38 +0530

    Updates

---
+++ precache-manifest.6571eba186f8e82f20fd38345d164972.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5a52377f30aa7cbe1ec",
+    "url": "/react-movie-library/static/css/main.b942271f.chunk.css"
+  },
+  {
+    "revision": "c5a52377f30aa7cbe1ec",
+    "url": "/react-movie-library/static/js/main.c5a52377.chunk.js"
+  },
+  {
+    "revision": "483a0879fa0e6e687539",
+    "url": "/react-movie-library/static/js/1.483a0879.chunk.js"
+  },
+  {
+    "revision": "cf352fbdde7ef144008d",
+    "url": "/react-movie-library/static/js/2.cf352fbd.chunk.js"
+  },
+  {
+    "revision": "d6af29b6d923dd8e1e03",
+    "url": "/react-movie-library/static/js/runtime~main.d6af29b6.js"
+  },
+  {
+    "revision": "af084f04ace9b5eb0a1401d3788ca4c3",
+    "url": "/react-movie-library/static/media/remove.af084f04.png"
+  },
+  {
+    "revision": "484a349b5684235078800563aafc3d2a",
+    "url": "/react-movie-library/static/media/movieBG.484a349b.jpg"
+  },
+  {
+    "revision": "f7ed3a6ea1792af14c9d17b03bfc917d",
+    "url": "/react-movie-library/index.html"
+  }
+];
